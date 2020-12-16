b488d3bba796972d5c16227118a04089630e5e9d
April Adams <illusionlace@gmail.com>
2020-12-16 12:24:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,3 +5,29 @@ class CashRegister
     @discount = discount
     @items = []
   end 
+  def new_item(title, price, quantity = 1)
+    self.total += price * quantity 
+    @last_price = price * quantity 
+    while quantity.positive? 
+      @items << title 
+      quantity -= 1
+    end 
+  end 
+  def apply_discount
+    if self.discount > 0 
+      self.total -= ((self.discount/100.0)*self.total)
+      good_message = "After the discount, the total comes to $#{self.total.to_i}."
+      good_message 
+    else 
+      error_message = "There is no discount to apply."
+      error_message
+    end 
+  end 
+  def items 
+    @items 
+  end 
+  def void_last_transaction
+    self.total -= @last_price
+    self.total 
+  end 
+end 
